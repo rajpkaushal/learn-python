edbf84b26cdec7f55f1b41b082843ffa70227596
Raj <rajp.kaushal@gmail.com>
2023-10-05 19:04:25 +0530

    asignment 4 in progress

--- ujjwal/a4/CS110-A4-Q8 08102023.py
+++ ujjwal/a4/CS110-A4-Q8 08102023.py
@@ -1,11 +1,29 @@
 # Print all Armstrong numbers between 1 to 500
-for x in range (501):
+# for x in range (1000001):
+#   n = x
+#   n1 = str(n)
+#   l = len(n1)
+#   sum = 0
+#   m = 0
+#   while n % 10 > 0:
+#     m = n%10
+#     sum = sum + (m**l)
+#     n = n//10
+#   if sum == x:
+    # print(x, "number is armstrong")
+  # else:
+  #   print(x, "this number is not armstrong")
+  
+  # alternate code
+  # Print all Armstrong numbers between 1 to 500
+for x in range (1000001):
   n = x
   n1 = str(n)
   l = len(n1)
   sum = 0
   m = 0
-  while n % 10 > 0:
+  
+  for i in range(l):
     m = n%10
     sum = sum + (m**l)
     n = n//10
